0d2125afc4725328d4c0a41e5c1f317fa5b55763
Mohammad Khan <mikhansd@gmail.com>
2026-09-16 17:20:40 -0700

    Update VM Port and IP

---
+++ Shared/Keys.h
@@ -0,0 +1,7 @@
+#pragma once
+inline constexpr int LOCAL_TCP_PORT = 5000;
+inline constexpr int LOCAL_VIDEO_PORT = 8554;
+inline constexpr const char* LOCAL_PI_IP = "192.168.8.176";
+inline constexpr int REMOTE_TCP_PORT = 15005;
+inline constexpr int REMOTE_VIDEO_PORT = 18080;
+inline constexpr const char* VM_PUBLIC_IP = "54.183.2.224";
